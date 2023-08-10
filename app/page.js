8225acd6e44d7434916196a07862c477945b3b92
root <root@DESKTOP-TMHO0NS>
2023-08-10 23:53:20 +0530

    imported

--- app/page.js
+++ app/page.js
@@ -1,113 +1,115 @@
-import Image from 'next/image'
+import Image from "next/image";
+import style from "./styles.css";
+import logo from "./media/MUN LOGO.png";
+import circle from "./media/Ellipse 1.svg";
+import bgGradient from "./media/backgroundGradient.png";
+import MyComponent from "./about/page";
 
+const Event = (details) => {
+  return (
+    <div className="mt-[1rem] gradient-zero mx-auto w-[70vw] h-[140px] rounded-lg p-[10px] ">
+      <div className="flex justify-center flex-col  gradient-zeroOne  rounded-md  w-[100%] h-[100%]">
+        <p className="text-white font-poppins text-center text-5xl align-middle ">
+          {details.name}
+        </p>
+      </div>
+    </div>
+  );
+};
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
+    <>
+      <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+      <link rel="preconnect" href="https://fonts.gstatic.com"></link>
+      {/* eslint-disable-next-line @next/next/no-page-custom-font */}
+      <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet"
+      ></link>
+
+      {/* HERO SECTION! */}
+
+      <div className="flex flex-col items-center justify-center my-auto h-screen">
+        <div className="">
+          <h1 className="text-[2rem] m-auto text-gradient-to-r font-poppins from-yellow-500 to-white bg-clip-text mun-gradient mun-typography">
+            SJBHSMUN 2023
+          </h1>
+        </div>
+        <Image
+          className=" w-80 md:w-6/12 h-auto md:h-200   "
+          src={logo}
+          alt=""
+        />
+        <Image
+          className=" w-80 md:w-6/12 h-auto md:h-200   "
+          src={circle}
+          alt=""
+        />
+
+        <div className="text-center my-5">
+          <p className="text-[#646464] font-poppins text-xl ">Welcome to our</p>
+          <p className="text-white font-poppins text-4xl tracking-[11px]">
+            11TH EDITION
+          </p>
+        </div>
+      </div>
+      {/* HERO SECTION END! */}
+
+      {/* DATE REVEAL SECTION */}
+      <div className="background-gradient text-center flex flex-col h-[100%]">
+        <p className="font-poppins text-white mb-2 ">JOIN US ON</p>
+        <p className="font-poppins mun-gradient text-5xl font-semibold mb-2">
+          OCTOBER
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+
+        <div className="flex m-auto gap-[3rem] mb-2">
+          <p className="font-poppins text-white ">23rd</p>
+          <p className="font-poppins text-white">24th</p>
+          <p className="font-poppins text-white">25th</p>
+        </div>
+
+        <div className="flex gap-[3rem] m-auto mb-2">
+          <p className="font-poppins mun-gradient text-5xl font-semibold">30</p>
+          <p className="font-poppins mun-gradient text-5xl font-semibold">12</p>
+          <p className="font-poppins mun-gradient text-5xl font-semibold">60</p>
+          <p className="font-poppins mun-gradient text-5xl font-semibold">30</p>
+        </div>
+
+        <div className="flex gap-[3rem] m-auto mb-2">
+          <p className="font-poppins text-white">Days</p>
+          <p className="font-poppins text-white">Hours</p>
+          <p className="font-poppins text-white">Minutes</p>
+          <p className="font-poppins text-white">Seconds</p>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      {/* DATE REVEAL END */}
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      {/* Committees Reveal section */}
+      <section className="m-3">
+        <div className="committees">
+          <div className="committees-background"></div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+          <div className=" flex flex-col gap-3 headingText">
+            <h1 className="font-poppins text-5xl text-center text-white ">
+              What are the
+            </h1>
+            <h1 className="font-poppins text-5xl text-center text-white tracking-[5px] ">
+              Committees?
+            </h1>
+          </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+          <div>
+            <Event name="Comittee Name" details="" />
+            <Event name="Comittee Name" details="" />
+            <Event name="Comittee Name" details="" />
+            <Event name="Comittee Name" details="" />
+            <Event name="Comittee Name" details="" />
+            <Event name="Comittee Name" details="" />
+            <MyComponent />
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
