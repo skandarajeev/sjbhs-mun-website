4458327be05393d75011a14dd024e318b0fbc6f1
root <root@DESKTOP-TMHO0NS>
2023-08-16 23:12:44 +0530

    modified to export

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { output: "export", images: { unoptimized: true } };
+const nextConfig = {
+  output: "export",
+  images: { unoptimized: true },
+  basePath: "/sjbhsmun",
+  assetPrefix:
+    process.env.NODE_ENV === "production"
+      ? "http://localhost:3000/sjbhsmun/"
+      : undefined,
+};
 
 module.exports = nextConfig;
