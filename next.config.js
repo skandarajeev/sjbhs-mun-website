b755593869b8f10be69dd412863a286c24f4d17a
skandarajeev <skandarajeev@gmail.com>
2023-08-18 19:01:31 +0530

    vercel ready

--- next.config.js
+++ next.config.js
@@ -1,12 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  images: { unoptimized: true },
-  basePath: process.env.NODE_ENV === "production" ? "/sjbhsmun" : "",
-  assetPrefix:
-    process.env.NODE_ENV === "production"
-      ? "http://localhost:3000/sjbhsmun/"
-      : "http://localhost:3000/",
+  //When you are ready to build a static export uncomment this
+  // output: "export",
+  // images: { unoptimized: true },
+  // basePath: process.env.NODE_ENV === "production" ? "/sjbhsmun" : "",
+  // assetPrefix:
+  //   process.env.NODE_ENV === "production"
+  //     ? "http://localhost:3000/sjbhsmun/"
+  //     : "http://localhost:3000/",
 };
 
 module.exports = nextConfig;
