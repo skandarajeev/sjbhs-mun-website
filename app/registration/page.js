e661bfb8783559a0cd3fb90cff1212106119ec61
root <root@DESKTOP-TMHO0NS>
2023-09-01 22:41:16 +0530

    delegation visible indi invisible

--- app/registration/page.js
+++ app/registration/page.js
@@ -4,7 +4,7 @@ import Head from "next/head";
 import { Indiregister, DelegationRegistration } from "../components/register";
 export default function page() {
   return (
-    <Indiregister />
-    //<DelegationRegistration />
+    //<Indiregister />
+    <DelegationRegistration />
   );
 }
